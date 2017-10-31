03164cb6ad3af85477f33b331d4ec61e3c1c3d96
BuildTools <gameanana@gmail.com>
2017-10-31 21:36:37 +0100

    🎁 Add the available places array

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,17 @@ class Board
 		@board = Array.new 9
 	end
 
+	# Place a piece at a position
+	def place(position, piece)
+		@board[postion] = piece
+	end
+
+	# Retrieves an array of available places indexes
+	def available_spaces
+		availlable = []
+		@board.size.times |i|
+			availlable << i if @board[i].nil?
+		end
+	end
+
 end
