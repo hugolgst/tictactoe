b4b36d7d03da6d6660c654940a4e022589ba07de
BuildTools <gameanana@gmail.com>
2017-10-31 21:48:38 +0100

    🔧 Changes tab

--- lib/board.rb
+++ lib/board.rb
@@ -1,20 +1,20 @@
 class Board
 
-	def initialize
-		@board = Array.new 9
-	end
+  def initialize
+    @board = Array.new 9
+  end
 
 	# Place a piece at a position
-	def place(position, piece)
-		@board[postion] = piece
-	end
+  def place(position, piece)
+    @board[postion] = piece
+  end
 
-	# Retrieves an array of available places indexes
-	def available_spaces
-		availlable = []
-		@board.size.times |i|
-			availlable << i if @board[i].nil?
-		end
-	end
+  # Retrieves an array of available places indexes
+  def available_spaces
+    availlable = []
+    @board.size.times |i|
+      availlable << i if @board[i].nil?
+    end
+  end
 
 end
