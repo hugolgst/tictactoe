632dd2ba848a5d312c78146a68eed9ee468af759
BuildTools <gameanana@gmail.com>
2017-11-02 10:21:49 +0100

    🎁 Add Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,16 @@
 class Player
 
+  def initialize board
+    @board = board
+  end
+
+  def place position
+    unless @board.empty_place? postion
+      puts "Invalid entry."
+      return
+    end
+
+    @board.place(position, "X")
+  end
+
 end
