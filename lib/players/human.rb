466bf922f0de4fe5b7dacc26bbca3629ff2aefab
Hugo <gameanana@gmail.com>
2018-02-17 21:44:39 +0100

    🔧 Put positions in order (@pandipanda41)

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -8,11 +8,11 @@ class Human < Player
     puts "It's your turn, enter a position."
     loop do
       position = gets.to_i
-      break if board.empty_place?(position) && (0..8) === position 
+      break if board.empty_place?(position) && (1..9) === position 
       puts 'Please enter a valid position'
     end
 
-    place(board, position)
+    place(board, position - 1)
     board.draw
   end
 
